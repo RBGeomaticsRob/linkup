cd8caee86db427ad6affc556af2ebdc34a5e0705
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-07 17:30:55 +0100

    Added test for password functionality

--- server.rb
+++ server.rb
@@ -1,16 +1,18 @@
 require 'sinatra/base'
 require 'data_mapper'
-require 'tag'
-require 'link'
 
 env = ENV['RACK_ENV'] || 'development'
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 require './lib/link' # needs to be done after setup hence the placing
+require './lib/user'
+require './lib/tag'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     @links = Link.all
@@ -25,6 +27,29 @@ class BookmarkManager < Sinatra::Base
     title = params['title']
     Link.create(url: url, title: title, tags: tags)
     redirect to('/')
+  end
+
+  get '/tags/:text' do
+    tag = Tag.first(text: params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
   end
   run! if app_file == $PROGRAM_NAME
 end
