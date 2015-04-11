9620bfa2c92b2245456c37ad168e3883e55a8448
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-11 15:16:42 +0100

    Adjusted placement of requires in server.rb to add to readability.

--- app/server.rb
+++ app/server.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'data_mapper'
 require 'rack-flash'
 require 'sinatra/partial'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
@@ -17,18 +18,17 @@ class BookmarkManager < Sinatra::Base
       @current_user ||= User.get(session[:user_id]) if session[:user_id]
     end
   end
-
+  require './app/controllers/homepage'
+  require './app/controllers/links'
+  require './app/controllers/sessions'
+  require './app/controllers/tags'
+  require './app/controllers/users'
   run! if app_file == $PROGRAM_NAME
 
 end
 
-require './app/models/link' # needs to be done after setup hence the placing
-require './app/models/user'
-require './app/models/tag'
-require './app/controllers/homepage'
-require './app/controllers/links'
-require './app/controllers/sessions'
-require './app/controllers/tags'
-require './app/controllers/users'
+# require './app/models/link' # needs to be done after setup hence the placing
+# require './app/models/user'
+# require './app/models/tag'
 
-require_relative 'data_mapper_setup'
+
